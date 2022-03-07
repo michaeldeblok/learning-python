f96e1a26b26104a69d5f951795392b9b9314e8b2
Michael de Blok <mblok@dropbox.com>
2022-03-07 10:05:58 -0500

    Adding learning scripts from Git and Learning Python

---
+++ Python/Ch2/modules_start.py
@@ -0,0 +1,16 @@
+# import the math module, which contains features for working with mathematics
+import math
+
+# the math module contains lots of pre-built functions
+print("The square root of 16", math.sqrt(16))
+
+# in addition to functions, some modules contain useful constants
+print("Pi is:", math.pi)
+
+# try some of the math functions for yourself here:
+
+def inc(a,b=1):
+    return(a+b)
+a=inc(1)
+a=inc(a,a)
+print(a)
---
+++ Python/Ch5/htmlparsing_start.py
@@ -0,0 +1,43 @@
+# 
+# Example file for parsing and processing HTML
+#
+
+from html.parser import HTMLParser
+
+class MyHTMLParser(HTMLParser):
+  # function to handle the ending tag
+  def handle_endtag(self, tag):
+    print ("Encountered an end tag:", tag)
+    pos = self.getpos()
+    print ("\tAt line: ", pos[0], " position ", pos[1])
+    
+  # function to handle character and text data (tag contents)
+  def handle_data(self, data):
+    if (data.isspace()):
+      return
+    print ("Encountered some text data:", data)
+    pos = self.getpos()
+    print ("\tAt line: ", pos[0], " position ", pos[1])
+  
+  # function to handle the processing of HTML comments
+  def handle_comment(self, data):
+    print ("Encountered comment:", data)
+    pos = self.getpos()
+    print ("\tAt line: ", pos[0], " position ", pos[1])
+
+
+def main():
+  # instantiate the parser and feed it some HTML
+  parser = MyHTMLParser()
+  f = open("samplehtml.html")
+  if f.mode == 'r':
+    contents = f.read()
+    parser.feed(contents)
+
+
+
+
+
+if __name__ == "__main__":
+  main();
+  
