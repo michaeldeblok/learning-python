0c7b4931c1a75fcc67221769d07809ef9beb998b
Michael de Blok <mblok@dropbox.com>
2022-03-09 10:17:28 -0500

    committing changes

--- Python Essential Training/Chap03/sequence.py
+++ Python Essential Training/Chap03/sequence.py
@@ -2,5 +2,44 @@
 # Copyright 2009-2017 BHG http://bw.org/
 
 x = [ 1, 2, 3, 4, 5 ]
+x[4] = 42
 for i in x:
     print('i is {}'.format(i))
+    print(type(x))
+
+x = range(5, 50, 5)
+for i in x:
+    print('i is {}'.format(i))
+    print(type(x))
+
+x = list(range(5))
+x[2] = 42
+for i in x:
+    print('i is {}'.format(i))
+    print(type(x))
+
+x = { 'one': 1, 'two': 2, 'three': 3, 'four': 4, 'five': 5}
+for i in x:
+    print('i is {}'.format(i))
+    print(type(x))
+
+x = { 'one': 1, 'two': 2, 'three': 3, 'four': 4, 'five': 5}
+for k, v in x.items():
+    print('k: {}, v: {}'.format(k, v))
+    print(type(x))
+
+
+x = (1, 'two', 3.0, [4, 'four'], 5)
+y = (1, 'two', 3.0, [4, 'four'], 5)
+
+
+if x[0] is y[0]:
+    print('yep')
+else:
+    print('nope')
+
+
+if isinstance(x, tuple):
+    print('yep')
+else:
+    print('nope')
--- Python Essential Training/Chap03/types.py
+++ Python Essential Training/Chap03/types.py
@@ -1,6 +1,27 @@
 #!/usr/bin/env python3
 # Copyright 2009-2017 BHG http://bw.org/
 
-x = 7
+#x = 'seven "{1:<09}" "{0:>09}"'.format(8, 9)
+# a = 8
+# b = 9
+# x = f'seven {a:<09} {b:>09}'
+# print('x is {}'.format(x))
+# print(type(x))
+
+
+# from decimal import *
+# a = Decimal('.10')
+# b = Decimal('.30')
+# x = a + a + a - b
+# print('x is {}'.format(x))
+# print(type(x))
+
+
+x = 'X'
 print('x is {}'.format(x))
 print(type(x))
+
+if x:
+    print('True')
+else:
+    print('False')
