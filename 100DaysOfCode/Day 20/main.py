4ca3cb23f5110c09600a0673fceeeae423390e34
Michael de Blok <mblok@dropbox.com>
2022-06-29 15:18:16 -0400

    adding files

--- 100DaysOfCode/Day 20/main.py
+++ 100DaysOfCode/Day 20/main.py
@@ -1,5 +1,7 @@
 from turtle import Screen
 from snake import Snake
+from food import Food
+from scoredboard import Scoreboard
 import time
 
 screen = Screen()
@@ -9,7 +11,8 @@ screen.title("My Snake Game")
 screen.tracer(0)
 
 snake = Snake()
-
+food = Food()
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(snake.up, "Up")
@@ -23,6 +26,26 @@ while game_is_on:
     time.sleep(0.1)
 
     snake.move()
+
+    # Detect collision with food
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        snake.extend()
+        scoreboard.increase_score()
+
+    # Detect collision with wall
+    if snake.head.xcor() > 300 or snake.head.xcor() < -300 or snake.head.ycor() > 300 or snake.head.ycor() < -300:
+        game_is_on = False
+        scoreboard.game_over()
+
+    # Detect collision with tail
+    for segment in snake.segments[1:-1]:
+        if snake.head.distance(segment) < 10:
+            game_is_on = False
+            scoreboard.game_over()
+    # If head collides with any segment in the tail:
+        # trigger game_over sequence
+
 
 
 screen.exitonclick()
